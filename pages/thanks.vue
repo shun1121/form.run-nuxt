dd9b568d31a969bdddb2b1bc86a7a49547b67d93
shun1121 <hikitanikun.ware80000@gmail.com>
2021-04-17 10:16:40 +0900

    added thanks.vue

---
+++ pages/thanks.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="12" md="12" class="mt-16">
+            <h1 align="center">
+                The messages have sent!
+            </h1>
+        </v-col>
+    </v-row>
+</template>
